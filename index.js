8df0dd3a7a79dd2520e84626db17d5c0028d500c
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-07-29 10:35:20 +1000

    added adding function & solve conflicts

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("I caused a merge conflict.");
+
+console.log("Hello world");
+
+var multiplyNums = function(x, y){
+    return "Sum of " + x + " and " + y + " is: " + (x + y);
+}
+console.log(multiplyNums(5, 10));
